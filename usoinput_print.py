98d5fda0c0c61596ee6d7b37dc36e5813e561f4b
sophiaymar <sophia.aymar7731@alumnos.udg.mx>
2026-09-22 21:38:23 -0600

    Delete usoinput_print.py

--- usoinput_print.py
+++
@@ -1,9 +0,0 @@
-nombre= input("¿Cual es tu nombre? ")
-print("Tu nombre es:",nombre)
-edad= input("¿Cuantos años tienes? ")
-print("Tienes", edad, "años")
-print(type(edad))
-estatura=input("Ingresa tu estatura en metros: ")
-print("Mides",estatura,"metros")
-print(type(estatura))
-
